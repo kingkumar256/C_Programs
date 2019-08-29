8908f5c4a1176c81c1ff5a3453a68f9f57283eac
kingkumar256 <54665452+kingkumar256@users.noreply.github.com>
2019-08-29 15:07:54 +0530

    Add files via upload

---
+++ conversions(binary2all)/binary2Decimal.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+int binary2decimal(num)
+{
+   
+ int rem,val,base;
+    
+base=1;
+    val=0;
+   
+ while(num>0)
+    {
+    
+    rem=num%10;
+       
+ val=val+base*rem;
+    
+    num=num/10;
+      
+  base=base*2;
+    }
+ 
+   return val;
+    
+}
+
+
+int main()
+{
+    
+int num,result;
+   
+ scanf("%d",&num);
+ 
+   result=binary2decimal(num);
+ 
+   printf(result);
+}
